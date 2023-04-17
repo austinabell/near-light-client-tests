f89d7b52efc89e62cc47f3814ce1c3604e6d317b
Austin Abell <austinabell8@gmail.com>
2023-04-17 16:45:12 -0400

    add random sampling script

---
+++ scripts/block_validation_random.ts
@@ -0,0 +1,65 @@
+import { JsonRpcProvider } from "near-api-js/lib/providers";
+import { validateLightClientBlock } from "../lib";
+
+async function main() {
+  const provider = new JsonRpcProvider({
+    url: "https://archival-rpc.mainnet.near.org",
+  });
+
+  const stat = await provider.status();
+
+  // Get block in at least the last epoch (epoch duration 43,200 blocks on mainnet and testnet)
+  const height = stat.sync_info.latest_block_height;
+  console.log("current height is " + height);
+
+  let minValid = height;
+  let maxInvalid = 0;
+  let firstEpochHeight;
+  let nextBlock;
+  let prevBlock;
+
+  while (true) {
+    try {
+      firstEpochHeight = Math.floor(Math.random() * height - 1);
+      console.log("querying for block at height: " + firstEpochHeight);
+      const firstBlock = await provider.block({ blockId: firstEpochHeight });
+      console.log("got block hash: " + firstBlock.header.hash);
+      prevBlock = await provider.nextLightClientBlock({
+        last_block_hash: firstBlock.header.hash,
+      });
+      console.log("last known: " + prevBlock.inner_lite.height);
+      nextBlock = await provider.nextLightClientBlock({
+        // TODO using prev block hash for convenience. Maybe there is a better way around this?
+        last_block_hash: prevBlock.prev_block_hash,
+      });
+      console.log("next block", nextBlock.inner_lite.height);
+      if (!prevBlock.next_bps) {
+        throw new Error("Rpc should include the next_bps field");
+      }
+
+      // This will throw an error if invalid
+      validateLightClientBlock(prevBlock, prevBlock.next_bps, nextBlock);
+      console.log(`validated block at height: ${nextBlock.inner_lite.height}`);
+      if (firstEpochHeight < minValid) {
+        console.log("NEW MIN VALID: " + firstEpochHeight);
+        minValid = firstEpochHeight;
+      }
+    } catch (e) {
+      console.warn("error: " + e);
+      if (e.toString().includes("DB Not Found Error: BLOCK HEIGHT:")) {
+      } else if (
+        e.toString().includes("Next block producers hash doesn't match")
+      ) {
+        console.log("BPS MISMATCH: ", prevBlock.next_bps);
+      } else {
+        if (firstEpochHeight > maxInvalid) {
+          console.log("NEW MAX INVALID: " + firstEpochHeight);
+          maxInvalid = firstEpochHeight;
+        }
+      }
+      continue;
+    }
+  }
+}
+
+main();
